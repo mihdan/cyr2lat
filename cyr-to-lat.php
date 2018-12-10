ac9d3cf01777242cfa69497fd25ae9ff6604e272
mihdan <mikhail@kobzarev.com>
2018-12-11 00:34:38 +0300

    Удаляем дублирование знаков дефиса в слагах постов при транслитерации. В начале и в конце - полностбю удаляем

--- cyr-to-lat.php
+++ cyr-to-lat.php
@@ -136,7 +136,15 @@ function ctl_sanitize_title( $title ) {
 
 	if ( empty( $term ) ) {
 		$title = strtr( $title, apply_filters( 'ctl_table', $iso9_table ) );
-		$title = preg_replace( "/[^A-Za-z0-9`'_\-\.]/", '-', $title );
+
+		if ( function_exists( 'iconv' ) ) {
+			$title = iconv( 'UTF-8', 'UTF-8//TRANSLIT//IGNORE', $title );
+		}
+
+		$title = preg_replace( "/[^A-Za-z0-9'_\-\.]/", '-', $title );
+		$title = preg_replace( '/\-+/', '-', $title );
+		$title = preg_replace( '/^-+/', '', $title );
+		$title = preg_replace( '/-+$/', '', $title );
 	} else {
 		$title = $term;
 	}
