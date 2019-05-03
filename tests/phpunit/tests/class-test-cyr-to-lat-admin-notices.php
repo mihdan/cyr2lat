6a6c50f00bbd5c2f4b4749f4eb8e936f5e7251f8
kagg-design <info@kagg.eu>
2019-05-03 16:30:39 +0300

    Add tests for Test_Cyr_To_Lat_Admin_Notices.

---
+++ tests/phpunit/tests/class-test-cyr-to-lat-admin-notices.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Test_Cyr_To_Lat_Admin_Notices class file
+ *
+ * @package cyr-to-lat
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class Test_Cyr_To_Lat_Admin_Notices
+ *
+ * @group admin-notices
+ */
+class Test_Cyr_To_Lat_Admin_Notices extends TestCase {
+
+	/**
+	 * Setup test
+	 */
+	public function setUp(): void {
+		parent::setUp();
+		\WP_Mock::setUp();
+	}
+
+	/**
+	 * End test
+	 */
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+		parent::tearDown();
+	}
+
+	/**
+	 * Test constructor
+	 *
+	 * @throws ReflectionException Reflection Exception.
+	 */
+	public function test_constructor() {
+		$classname = 'Cyr_To_Lat_Admin_Notices';
+
+		// Get mock, without the constructor being called.
+		$mock = $this->getMockBuilder( $classname )->disableOriginalConstructor()->getMock();
+
+		// Set expectations for constructor calls.
+		\WP_Mock::expectActionAdded( 'admin_notices', array( $mock, 'show_notices' ) );
+
+		// Now call the constructor.
+		$reflected_class = new ReflectionClass( $classname );
+		$constructor     = $reflected_class->getConstructor();
+		$constructor->invoke( $mock );
+
+		$this->assertTrue( true );
+	}
+
+	/**
+	 * Test add_notice() and show_notices()
+	 */
+	public function test_add_show_notices() {
+		$expected = '			<div class="notice">
+				<p>
+					<strong>
+						First message					</strong>
+				</p>
+			</div>
+						<div class="error">
+				<p>
+					<strong>
+						Second message					</strong>
+				</p>
+			</div>
+			';
+
+		$subject = new Cyr_To_Lat_Admin_Notices();
+
+		\WP_Mock::passthruFunction( 'wp_kses_post' );
+
+		$subject->add_notice( 'First message' );
+		$subject->add_notice( 'Second message', 'error' );
+
+		ob_start();
+		$subject->show_notices();
+		$result = ob_get_clean();
+
+		$this->assertSame( $expected, $result );
+	}
+}
