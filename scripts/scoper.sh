4db71dc8d054d3f35546c3d8f6c5f618298546a8
kagg-design <info@kagg.eu>
2019-12-08 20:49:14 +0200

    Make scoper.sh run under 7.3 and 7.4 only.

--- scripts/scoper.sh
+++ scripts/scoper.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # This script runs the php-scoper, and overwrites the embedded wp-background-processing library inside of Cyr_To_Lat.
 
+PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
+
+if [[ $PHP_VERSION != "7.3" && $PHP_VERSION != "7.4" ]]; then
+	echo "This script must be used inder php 7.3 or 7.4 only"
+	exit
+fi
+
 SCRIPTDIR="$(dirname "$0")"
 CYR2LAT_PLUGIN_PATH="${1:-$SCRIPTDIR/..}"
 
@@ -21,8 +28,12 @@ fi
 
 rm -rf ${CYR2LAT_BUILD_PATH:?}/*
 
+composer require humbug/php-scoper:dev-master --ignore-platform-reqs
+
 vendor/humbug/php-scoper/bin/php-scoper add-prefix -vv --no-interaction --prefix=$TWIG_SCOPER_PREFIX --config=scripts/wp-background-processing.scoper.inc.php
 vendor/squizlabs/php_codesniffer/bin/phpcbf --standard=phpcs.xml build
+
+composer remove humbug/php-scoper --ignore-platform-reqs
 
 rm -rf ${CYR2LAT_LIB_PATH:?}/*
 cp -r ${CYR2LAT_BUILD_PATH}/* ${CYR2LAT_LIB_PATH}
