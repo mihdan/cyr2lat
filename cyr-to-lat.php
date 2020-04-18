ff3413e54823394f7dea7f78d2f7145c2f1e09c6
kagg-design <info@kagg.eu>
2020-04-18 14:23:36 +0300

    Bump up version to 4.4.0.

--- cyr-to-lat.php
+++ cyr-to-lat.php
@@ -9,8 +9,8 @@
  * Author URI: https://profiles.wordpress.org/sergeybiryukov/
  * Requires at least: 5.1
  * Tested up to: 5.4
- * Version: 4.3.5
- * Stable tag: 4.3.5
+ * Version: 4.4.0
+ * Stable tag: 4.4.0
  *
  * Text Domain: cyr2lat
  * Domain Path: /languages/
@@ -34,7 +34,7 @@ if ( defined( 'CYR_TO_LAT_VERSION' ) ) {
 /**
  * Plugin version.
  */
-define( 'CYR_TO_LAT_VERSION', '4.3.5' );
+define( 'CYR_TO_LAT_VERSION', '4.4.0' );
 
 /**
  * Path to the plugin dir.
