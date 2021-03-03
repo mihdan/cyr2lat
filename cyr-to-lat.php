3e67f8c6e9af21a40bc42ab2a56abba28c20f510
KAGG Design <info@kagg.eu>
2021-03-03 16:47:25 +0200

    Comb plugin header in accordance with wp.org standard.

--- cyr-to-lat.php
+++ cyr-to-lat.php
@@ -2,21 +2,23 @@
 /**
  * Cyr-To-Lat
  *
- * Plugin Name: Cyr-To-Lat
- * Plugin URI: https://wordpress.org/plugins/cyr2lat/
- * Description: Converts Cyrillic characters in post and term slugs to Latin characters. Useful for creating human-readable URLs. Based on the original plugin by Anton Skorobogatov.
- * Author: Sergey Biryukov, Mikhail Kobzarev, Igor Gergel
- * Author URI: https://profiles.wordpress.org/sergeybiryukov/
+ * @package           cyr-to-lat
+ * @author            Sergey Biryukov, Mikhail Kobzarev, Igor Gergel
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Cyr-To-Lat
+ * Plugin URI:        https://wordpress.org/plugins/cyr2lat/
+ * Description:       Converts Cyrillic characters in post and term slugs to Latin characters. Useful for creating human-readable URLs. Based on the original plugin by Anton Skorobogatov.
+ * Version:           4.6.4
  * Requires at least: 5.1
- * Tested up to: 5.7
- * Version: 4.6.4
- * Stable tag: 4.6.4
- *
- * Text Domain: cyr2lat
- * Domain Path: /languages/
- *
- * @package cyr-to-lat
- * @author  Sergey Biryukov, Mikhail Kobzarev, Igor Gergel
+ * Requires PHP:      5.6.20
+ * Author:            Sergey Biryukov, Mikhail Kobzarev, Igor Gergel
+ * Author URI:        https://profiles.wordpress.org/sergeybiryukov/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       cyr2lat
+ * Domain Path:       /languages/
  */
 
 namespace Cyr_To_Lat;
