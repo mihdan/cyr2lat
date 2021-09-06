49f1397d1413855eb1c1a7b210d22bdcf6bbcf92
KAGG Design <info@kagg.eu>
2021-09-06 15:05:01 +0300

    Bump up version to 5.2.2.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -42,7 +42,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'CYR_TO_LAT_TEST_VERSION', '5.2.1' );
+define( 'CYR_TO_LAT_TEST_VERSION', '5.2.2' );
 
 /**
  * Path to the plugin dir.
