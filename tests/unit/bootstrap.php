b25976bcf2cdfaf3a6b8a447159707a8a7e85800
kagg-design <info@kagg.eu>
2023-07-17 01:20:05 +0300

    Fix bootstrap.php.

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -89,7 +89,7 @@ FunctionMocker::init(
 		'whitelist'             => [
 			realpath( PLUGIN_PATH . '/cyr-to-lat.php' ),
 			realpath( PLUGIN_PATH . '/src/php' ),
-			realpath( PLUGIN_PATH . '/tests/unit/stubs' ),
+			realpath( PLUGIN_PATH . '/tests/unit/Stubs' ),
 		],
 		'redefinable-internals' => [
 			'class_exists',
