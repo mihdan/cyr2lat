d28e0191a98083226470066722e5d8110e24c343
KAGG Design <info@kagg.eu>
2026-02-08 18:11:45 +0200

    Bump version to 6.7.0.

--- cyr-to-lat.php
+++ cyr-to-lat.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Cyr-To-Lat
  * Plugin URI:        https://kagg.eu/cyr-to-lat/
  * Description:       Convert Non-Latin characters in post and term slugs to Latin characters. Useful for creating human-readable URLs. Based on the original plugin by Anton Skorobogatov.
- * Version:           6.7.0-RC1
+ * Version:           6.7.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Sergey Biryukov, Mikhail Kobzarev, Igor Gergel
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-const CYR_TO_LAT_VERSION = '6.7.0-RC1';
+const CYR_TO_LAT_VERSION = '6.7.0';
 
 /**
  * Path to the plugin dir.
