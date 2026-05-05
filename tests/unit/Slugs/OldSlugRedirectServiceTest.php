b06bd96049ecc2375699c76555f551d9788b2367
KAGG Design <info@kagg.eu>
2026-05-06 01:26:24 +0300

    Implement task 05.03 old slug regression tests

--- tests/unit/Slugs/OldSlugRedirectServiceTest.php
+++ tests/unit/Slugs/OldSlugRedirectServiceTest.php
@@ -28,8 +28,8 @@ class OldSlugRedirectServiceTest extends CyrToLatTestCase {
 	public function test_check_for_changed_slugs_boundary(): void {
 		$transliterator = Mockery::mock( Transliterator::class );
 		$subject        = new OldSlugRedirectService( $transliterator );
-		$post        = (object) [ 'post_name' => 'j' ];
-		$post_before = (object) [ 'post_name' => 'j' ];
+		$post           = (object) [ 'post_name' => 'j' ];
+		$post_before    = (object) [ 'post_name' => 'j' ];
 
 		$subject->check_for_changed_slugs( 5, $post, $post_before );
 
@@ -61,6 +61,100 @@ class OldSlugRedirectServiceTest extends CyrToLatTestCase {
 
 		WP_Mock::userFunction( 'get_post_type' )->with( $post )->andReturn( 'post' );
 		WP_Mock::userFunction( 'is_post_type_hierarchical' )->with( 'post' )->andReturn( false );
+
+		$subject->check_for_changed_slugs( 5, $post, $post_before );
+
+		self::assertSame( '%D0%B9', $post_before->post_name );
+	}
+
+	/**
+	 * Test check_for_changed_slugs() skips unsupported post shapes.
+	 *
+	 * @param object $post        The post object.
+	 * @param object $post_before The previous post object.
+	 *
+	 * @return void
+	 * @dataProvider dp_test_check_for_changed_slugs_skips_unsupported_post_shapes
+	 */
+	public function test_check_for_changed_slugs_skips_unsupported_post_shapes( object $post, object $post_before ): void {
+		$transliterator = Mockery::mock( Transliterator::class );
+		$transliterator->shouldNotReceive( 'transliterate' );
+
+		$subject = new OldSlugRedirectService( $transliterator );
+
+		WP_Mock::userFunction( 'get_post_type' )->with( $post )->andReturn( $post->post_type );
+		WP_Mock::userFunction( 'is_post_type_hierarchical' )->with( $post->post_type )
+			->andReturn( 'page' === $post->post_type );
+
+		$subject->check_for_changed_slugs( 5, $post, $post_before );
+
+		self::assertSame( 'й', $post_before->post_name );
+	}
+
+	/**
+	 * Data provider for test_check_for_changed_slugs_skips_unsupported_post_shapes().
+	 *
+	 * @return array
+	 */
+	public static function dp_test_check_for_changed_slugs_skips_unsupported_post_shapes(): array {
+		return [
+			'non-published post'  => [
+				(object) [
+					'post_name'   => 'j',
+					'post_status' => 'draft',
+					'post_type'   => 'post',
+				],
+				(object) [
+					'post_name' => 'й',
+				],
+			],
+			'hierarchical post'   => [
+				(object) [
+					'post_name'   => 'j',
+					'post_status' => 'publish',
+					'post_type'   => 'page',
+				],
+				(object) [
+					'post_name' => 'й',
+				],
+			],
+			'attachment mismatch' => [
+				(object) [
+					'post_name'   => 'j',
+					'post_status' => 'draft',
+					'post_type'   => 'attachment',
+				],
+				(object) [
+					'post_name' => 'й',
+				],
+			],
+		];
+	}
+
+	/**
+	 * Test check_for_changed_slugs() handles attachments with inherit status.
+	 *
+	 * @return void
+	 */
+	public function test_check_for_changed_slugs_handles_attachment_inherit_status(): void {
+		$transliterator = Mockery::mock( Transliterator::class );
+		$transliterator->shouldReceive( 'transliterate' )->with( 'й' )->andReturn( 'j' );
+
+		$subject = new OldSlugRedirectService( $transliterator );
+
+		$post = (object) [
+			'post_title'  => 'й',
+			'post_name'   => 'j',
+			'post_status' => 'inherit',
+			'post_type'   => 'attachment',
+		];
+
+		$post_before = (object) [
+			'post_name' => '',
+		];
+
+		WP_Mock::userFunction( 'get_post_type' )->with( $post )->andReturn( 'attachment' );
+		WP_Mock::userFunction( 'is_post_type_hierarchical' )->with( 'attachment' )->andReturn( false );
 
 		$subject->check_for_changed_slugs( 5, $post, $post_before );
 
